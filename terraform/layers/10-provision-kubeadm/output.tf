4f746b936becd4f759b56f78117f7946a75b8fae
csning1998 <january20070120@gmail.com>
2025-11-26 01:11:57 +0800

    fix(kubeadm): Restore the output in output.tf to make the kubeconfig available to other layers or for user access.

--- terraform/layers/10-provision-kubeadm/output.tf
+++ terraform/layers/10-provision-kubeadm/output.tf
@@ -3,3 +3,9 @@ output "kubeadm_pod_subnet" {
   description = "The CIDR for the Pod network."
   value       = var.kubeadm_cluster_config.pod_subnet
 }
+
+output "kubeconfig_content" {
+  description = "The content of the kubeconfig file."
+  value       = lookup(data.external.fetched_kubeconfig.result, "content", "kubeconfig-not-found")
+  sensitive   = true
+}
