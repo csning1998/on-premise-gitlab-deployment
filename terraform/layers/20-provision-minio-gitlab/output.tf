ce6d57adf63454d18248087fbd42546618ff4759
csning1998 <january20070120@gmail.com>
2025-11-28 00:51:41 +0800

    refactor(minio): Verified functionality within the `gitlab` service environment with refactored terraform minio module.

---
+++ terraform/layers/20-provision-minio-gitlab/output.tf
@@ -0,0 +1,19 @@
+
+output "gitlab_minio_ip_list" {
+  description = "List of MinIO node IPs for GitLab"
+  value = [
+    for node in var.minio_cluster_config.nodes.minio : node.ip
+  ]
+}
+
+output "gitlab_minio_haproxy_ip_list" {
+  description = "List of MinIO HAProxy node IPs for GitLab"
+  value = [
+    for node in var.minio_cluster_config.nodes.haproxy : node.ip
+  ]
+}
+
+output "gitlab_minio_virtual_ip" {
+  description = "MinIO virtual IP for GitLab"
+  value       = var.minio_cluster_config.ha_virtual_ip
+}
