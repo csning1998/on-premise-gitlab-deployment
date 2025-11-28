f8d08b65e2a9e4fd47105832bd5c75073a8b7e33
csning1998 <january20070120@gmail.com>
2025-11-28 22:08:55 +0800

    fix: follow the last path typo fix

--- terraform/layers/50-provision-kubeadm-gitlab/main.tf
+++ terraform/layers/50-provision-kubeadm-gitlab/main.tf
@@ -1,6 +1,6 @@
 
 module "kubeadm_gitlab" {
-  source = "../../modules/51-conposition-kubeadm-ha"
+  source = "../../modules/51-composition-kubeadm-ha"
 
   kubeadm_cluster_config = var.gitlab_cluster_config
   libvirt_infrastructure = var.gitlab_infrastructure
