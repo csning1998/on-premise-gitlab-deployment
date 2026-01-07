14735c12373f4213bb9ab35a97f68cfb7802d990
csning1998 <csning1998@gmail.com>
2026-01-07 17:15:36 +0800

    refactor(GitLab): Reorder Terraform Layer and Module Numbering Sequence (#45)
    ### Core Architecture
    
    1.  **Layer Resequencing**: The `gitlab-platform` layer has been moved from index `60` to `70` to reflect its position as a high-level application layer.
    
    2.  **Module Promotion**: The Kubeadm High Availability module (`kubeadm-ha`) has been promoted from `51` to `61`.

--- terraform/layers/60-gitlab-kubeadm/provider.tf
+++ terraform/layers/60-gitlab-kubeadm/provider.tf
@@ -20,5 +20,7 @@ provider "libvirt" {
 }
 
 provider "vault" {
-  # Vault server address is read from the VAULT_ADDR environment variable
+  address      = "https://${data.terraform_remote_state.vault_core.outputs.vault_ha_virtual_ip}:443"
+  ca_cert_file = abspath("${path.root}/../10-vault-core/tls/vault-ca.crt")
+  token        = jsondecode(file(abspath("${path.root}/../../../ansible/fetched/vault/vault_init_output.json"))).root_token
 }
