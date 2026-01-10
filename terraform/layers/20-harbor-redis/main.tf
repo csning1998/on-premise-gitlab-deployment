178cae37f1e9eb0ea606c36749a4309ca5d5af59
csning1998 <january20070120@gmail.com>
2026-01-11 01:34:39 +0800

    refactor(Harbor): Align Harbor Redis configurations with GitLab Redis to prepare for potential TLS support in future Harbor Helm Chart versions.

--- terraform/layers/20-harbor-redis/main.tf
+++ terraform/layers/20-harbor-redis/main.tf
@@ -2,11 +2,11 @@
 module "redis_harbor" {
   source = "../../modules/25-redis-ha"
 
-  topology_config = var.harbor_redis_compute
-  infra_config    = var.harbor_redis_infra
-
+  topology_config   = var.harbor_redis_compute
+  infra_config      = var.harbor_redis_infra
+  service_domain    = local.service_domain
+  vault_role_name   = local.vault_role_name
   vault_ca_cert_b64 = filebase64("${path.root}/../10-vault-core/tls/vault-ca.crt")
-  vault_role_name   = "harbor-redis-role"
 
   // Harbor presently doesnt support Redis Clusters or TLS based connections.
   // Ref: https://goharbor.io/docs/main/install-config/harbor-ha-helm/
