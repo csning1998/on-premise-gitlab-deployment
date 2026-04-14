2bf0bc141bba9c9d805e0a74dccbde80a0a9e3d7
csning1998 <csning1998@gmail.com>
2026-04-14 15:11:13 +0800

    fix(coredns): add `k8s-app` label to coredns configmap metadata.

--- terraform/modules/kubernetes-addons/coredns-config/resources.tf
+++ terraform/modules/kubernetes-addons/coredns-config/resources.tf
@@ -3,6 +3,9 @@ resource "kubernetes_config_map" "coredns_custom_config" {
   metadata {
     name      = "coredns"
     namespace = "kube-system"
+    labels = {
+      "k8s-app" = "kube-dns"
+    }
   }
 
   data = {
