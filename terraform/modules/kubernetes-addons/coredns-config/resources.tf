3a8c911378fd81f73a5a0eb04dd8655dd3dad961
csning1998 <csning1998@gmail.com>
2026-04-14 15:00:53 +0800

    fix(tf/harbor/microk8s): migrate coredns config resource from `kubernetes_config_map_v1_data` to `kubernetes_config_map`
    Because `v1_data` is a patch-style resource, the target must already exist. It does not manage metadata (other than the name used for locating) or the resource lifecycle. Since L50 is intended to reconfigure the resource, it has been changed back to a `kubernetes_config_map` resource.

--- terraform/modules/kubernetes-addons/coredns-config/resources.tf
+++ terraform/modules/kubernetes-addons/coredns-config/resources.tf
@@ -1,12 +1,9 @@
 
-resource "kubernetes_config_map_v1_data" "coredns_custom_config" {
-
+resource "kubernetes_config_map" "coredns_custom_config" {
   metadata {
     name      = "coredns"
     namespace = "kube-system"
   }
-
-  force = true
 
   data = {
     Corefile = local.final_corefile
