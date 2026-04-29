eeb2ff5282d086c0c681af14dba4261f01801dd3
csning1998 <csning1998@gmail.com>
2026-04-30 01:17:12 +0800

    fix(tf/harbor-oci): add `depends_on` to ensure Ansible Playbook executes only after Harbor resources are created

--- terraform/layers/40-provision-harbor-bootstrapper-frontend/resources.tf
+++ terraform/layers/40-provision-harbor-bootstrapper-frontend/resources.tf
@@ -71,7 +71,9 @@ module "ansible_sync_oci" {
 
   depends_on = [
     vault_kv_secret_v2.robot_helm_creds,
-    harbor_project.proxy_oci
+    harbor_project.proxy_oci,
+    harbor_project.proxy_projects,
+    harbor_registry.proxy_registries
   ]
 
   status_trigger = local.state.harbor_bootstrapper.topology_node
