4b97b00d0dee9dddacfbd0d220cca4e011887570
csning1998 <csning1998@gmail.com>
2026-05-02 01:27:43 +0800

    feat(tf/keycloak): initialize new Keycloak frontend infrastructure layer with necessary Terraform configuration and dependencies

---
+++ terraform/layers/30-infra-keycloak-frontend/main.tf
@@ -0,0 +1,25 @@
+
+module "keycloak_cluster" {
+  source = "../../middleware/ha-service-kvm-general"
+
+  # Identity & Service Definitions
+  svc_identity    = local.svc_identity
+  node_identities = local.node_identities
+
+  # Topology (Compute & Storage)
+  topology_cluster           = local.topology_cluster
+  storage_infrastructure_map = local.state.volume.storage_infrastructure_map
+
+  # Network Infrastructure
+  network_infrastructure_map = local.network_infrastructure_map
+
+  # Security & Credentials
+  credentials_system            = local.sec_vm_creds
+  security_vault_agent_identity = local.sec_vault_agent_identity
+
+  # Ansible Configuration
+  ansible_generic_config = {
+    template_vars = local.ansible_template_vars
+    extra_vars    = local.ansible_extra_vars
+  }
+}
