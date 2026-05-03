464bc5568865501d783ca0ab6e7dcb2fa0c91e05
csning1998 <csning1998@gmail.com>
2026-05-03 17:27:29 +0800

    fix(terraform/reloader): return `null` for reloader helm release metadata when disabled

--- terraform/modules/kubernetes-addons/reloader/outputs.tf
+++ terraform/modules/kubernetes-addons/reloader/outputs.tf
@@ -1,13 +1,13 @@
 
 output "helm_release_metadata" {
   description = "Basic metadata of the Reloader Helm release"
-  value = {
+  value = var.enabled ? {
     name        = helm_release.reloader[0].metadata.name
     version     = helm_release.reloader[0].metadata.version
     app_version = helm_release.reloader[0].metadata.app_version
     revision    = helm_release.reloader[0].metadata.revision
     status      = helm_release.reloader[0].status
-  }
+  } : null
 }
 
 output "namespace" {
